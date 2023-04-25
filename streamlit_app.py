3a9a785dafd19f08aaaf1b09867683f067f36dc7
Sorphex <8199718+Sorphex@users.noreply.github.com>
2023-04-25 13:09:20 +0200

    Update streamlit_app.py
    Change the ' to "

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mon's New Healthier Diner')
+streamlit.title("My Mon's New Healthier Diner")
 streamlit.header(' Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
