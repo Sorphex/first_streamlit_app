b20c656bf6c0b25a950f5b73fa774957c3bad5cb
Sorphex <8199718+Sorphex@users.noreply.github.com>
2023-04-25 13:06:01 +0200

    Update streamlit_app.py
    Added reciepies

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthier Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
