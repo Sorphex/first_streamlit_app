d5cb68bbd4663d05c115420dea145ab10a71f2c6
Sorphex <8199718+Sorphex@users.noreply.github.com>
2023-04-25 13:14:47 +0200

    Update streamlit_app.py
    Added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,7 @@ streamlit.text('🐔Hard-boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
+
